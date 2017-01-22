90f07068274ee0683bc896aa05e40b43898f9895
Hugo Lackermaier <hugolackermaier@MacBook-Pro-de-Hugo.local>
2017-01-22 13:42:10 +0100

    ok

---
+++ Sources/gameover.cpp
@@ -0,0 +1,29 @@
+//
+// Created by Hugo Lackermaier on 22/01/2017.
+//
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include "../Headers/Timer.h"
+
+
+void gameover(Timer T, float width, float height, sf::RenderWindow &window)
+{
+    sf::Font font;
+    sf::Text txt;
+    sf::Text txt_Game_Over;
+
+    txt.setFont(font);
+    txt.setCharacterSize(100);
+    txt.setColor(sf::Color::Red);
+    txt.setString(std::to_string(T.elapsedTime()));
+    txt.setPosition(width / 2 - 100, height / 3 * 1);
+
+    txt.setFont(font);
+    txt.setCharacterSize(100);
+    txt.setColor(sf::Color::Red);
+    txt.setString("GAME OVER");
+    txt.setPosition(width / 2 - 100, height / 3 * 2);
+
+    window.draw(txt);
+    window.draw(txt_Game_Over);
+}
