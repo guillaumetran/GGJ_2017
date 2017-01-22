2d8b1d4d52ddc6e51f470d76163f9ac1f5ee22b8
Guillaume Tran <guillaume.tran@epitech.eu>
2017-01-22 01:23:44 +0100

    update curve to act like an equalizer

--- Headers/Curve.h
+++ Headers/Curve.h
@@ -14,16 +14,18 @@ class Curve {
 public:
     int   _x;
     int   _y;
+    int   _nextDrop;
     float _deg;
     bool  _left;
 public:
     Curve(int y, int pos, bool left) : _y(pos), _left(left) {
-        _deg = _y;
+        _deg = 0;
+        _nextDrop = rand() % 150 + 10;
     }
     virtual ~Curve() {}
 
     void draw(sf::RenderWindow &window) {
-        sf::CircleShape circle(2);
+        sf::CircleShape circle(4);
         circle.setFillColor(sf::Color::Red);
         circle.setPosition(_x, _y);
         circle.setOrigin(2, 2);
@@ -32,12 +34,25 @@ public:
 
     void    update() {
         int offset;
-        _deg += 0.05;
-        if (_left)
-            offset = 50;
-        else
-            offset = 420;
-        _x = (sin(_deg) * 40) + offset;
+
+        _nextDrop--;
+        if (_nextDrop <= 0) {
+            _nextDrop = rand() % 150 + 10;
+            _deg = rand() % 70;
+            if (!_left)
+                _deg *= -1;
+        }
+        if (_left) {
+            offset = 0;
+            if (_deg > 0)
+                _deg -= 0.5;
+        }
+        else {
+            offset = 480;
+            if (_deg < 0)
+                _deg += 0.5;
+        }
+        _x = _deg + offset;
     }
 };
 
